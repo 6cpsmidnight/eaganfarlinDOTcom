2093b8f7a8088978a77c6d3f2cb9e813d854f9b6
Cyber1012 <55567338+Cyber1012@users.noreply.github.com>
2020-08-17 14:04:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Notes
 alert("Note: This Beta Is Heavily Under Development, And Is Not Ready For Mobile.");
-alert("Note: The Decorations/Styling Has Not Been Done Yet, And That Is Why It Looks Ugly.");
 
 // Code For Website
 $("body").ready(function() {
