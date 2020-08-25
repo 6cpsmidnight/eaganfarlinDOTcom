1650023763158204e6a1b91298276a9f5d5daef5
Cyber Coder <cyberelectro101@gmail.com>
2020-08-25 16:29:40 -0400

    Added Footer, changed Github link in the Navbar and added a icon to it.

--- External-Code/index.js
+++ External-Code/index.js
@@ -21,7 +21,7 @@ $("body").hover(function() {
   while (i < 5) {
 
     $(".arrow-down").fadeToggle(1000).fadeToggle(1000);
-    
+
     i++;
 
   }
@@ -44,3 +44,46 @@ $(window).scroll(function() {
   }
 
 });
+
+// $(".a-navbar-github-icon").click(function () {
+//
+//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
+//   window.open("https://github.com/Cyber1012");
+//
+// });
+//
+// $(".footer-icons").click(function () {
+//
+//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
+//
+// });
+//
+// $(".footer-icon-github").click(function () {
+//
+//   window.open("https://github.com/Cyber1012");
+//
+// });
+//
+// $(".footer-icon-gmail").click(function () {
+//
+//   window.open("mailto:farlineagan@gmail.com");
+//
+// });
+//
+// $(".footer-site-info-icons").click(function () {
+//
+//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
+//
+// });
+//
+// $(".footer-site-info-icons-youtube").click(function () {
+//
+//   window.open("https://www.youtube.com/channel/UCn8p9E5e_yQO24g1Z3F7mYg");
+//
+// });
+//
+// $(".footer-site-info-icons-pintrest").click(function () {
+//
+//   window.open("https://www.pintrest.com/thislittleshadow");
+//
+// });
