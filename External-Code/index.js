4ebf0756fb2a005e1035585bc199fd9381cb05fc
Cyber Coder <cyberelectro101@gmail.com>
2020-08-26 11:24:52 -0400

    Added a copy link button to the Footer. Changed the text in the about me section. Gave some more whitesoace between the Navbar and list in list of websites. Removed some commands from the Javascript.

--- External-Code/index.js
+++ External-Code/index.js
@@ -3,6 +3,8 @@ alert("This site is under heavy development and is not ready for mobile.");
 
 var yLevel = window.scrollY;
 
+console.log("For testing purposes: your Y Level is " + yLevel + ".");
+
 var i = 0;
 
 while (i < 5) {
@@ -31,7 +33,8 @@ $("body").hover(function() {
 $(window).scroll(function() {
 
   yLevel = window.scrollY;
-  console.log(yLevel);
+
+  console.log("For testing purposes: your Y Level is " + yLevel + ".");
 
   if (yLevel > 629) {
 
@@ -45,45 +48,8 @@ $(window).scroll(function() {
 
 });
 
-// $(".a-navbar-github-icon").click(function () {
-//
-//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
-//   window.open("https://github.com/Cyber1012");
-//
-// });
-//
-// $(".footer-icons").click(function () {
-//
-//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
-//
-// });
-//
-// $(".footer-icon-github").click(function () {
-//
-//   window.open("https://github.com/Cyber1012");
-//
-// });
-//
-// $(".footer-icon-gmail").click(function () {
-//
-//   window.open("mailto:farlineagan@gmail.com");
-//
-// });
-//
-// $(".footer-site-info-icons").click(function () {
-//
-//   alert("Thanks to Picture Perfect on Flaticon for this Icon!");
-//
-// });
-//
-// $(".footer-site-info-icons-youtube").click(function () {
-//
-//   window.open("https://www.youtube.com/channel/UCn8p9E5e_yQO24g1Z3F7mYg");
-//
-// });
-//
-// $(".footer-site-info-icons-pintrest").click(function () {
-//
-//   window.open("https://www.pintrest.com/thislittleshadow");
-//
-// });
+$(".footer-icon-copy-url").click(function() {
+
+  prompt("Copy Me", "https://www.eaganfarlin.com");
+
+});
