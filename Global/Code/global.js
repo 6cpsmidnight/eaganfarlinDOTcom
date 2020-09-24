ce6442a811cd98f7fb4eadec0bba65454317d776
Cyber Coder <cyberelectro1012@gmail.com>
2020-09-24 10:35:10 -0400

    Made my HTML code a lil' more clean.

--- Global/Code/global.js
+++ Global/Code/global.js
@@ -1,5 +1,7 @@
 // Notes
 alert("Only the home page is properly styled, the rest is in heavy development.");
+// Loading Screen Fades Out
+$("#loading-screen").fadeOut(1000);
 
 // Copy URL's URL Copier
 $(".footer-icon-copy-url").click(function () {
