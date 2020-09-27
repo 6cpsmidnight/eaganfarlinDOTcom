12e48d2a26c0d87be6f088b6a4b2937c5a19618b
Cyber Coder <cyberelectro1012@gmail.com>
2020-09-26 21:47:11 -0400

    Bye bye about me link.
    WIthout any way I see of responsively linking the about me link on the NavBar, I must say good bye to it.

--- Global/Code/global.js
+++ Global/Code/global.js
@@ -10,37 +10,3 @@ $(".footer-icon-copy-url").click(function () {
   prompt("Copy Me", "https://www.eaganfarlin.com/");
 
 });
-
-// Navigation Bar - About Me Link
-$(".navbar-about-link").click(function () {
-
-  // Checking What You Screen Width Is
-  let widthOfScreen = $(window).width();
-
-  if (widthOfScreen > 992) {
-
-    window.scrollTo({
-      top: 650,
-      left: 0,
-      behavior: "smooth"
-    });
-
-  } else if (widthOfScreen < 992 && widthOfScreen > 767) {
-
-    window.scrollTo({
-      top: 600,
-      left: 0,
-      behavior: "smooth"
-    });
-
-  } else if (widthOfScreen < 768) {
-
-    window.scrollTo({
-      top: 590,
-      left: 0,
-      behavior: "smooth"
-    });
-
-  }
-
-})
