e03038aaca967dc5f138c37c8852fc0abd905d75
Cyber Coder <cyberelectro101@gmail.com>
2020-10-20 19:30:59 -0400

    Bug fix for last update.

--- global/code/global.js
+++ global/code/global.js
@@ -141,6 +141,6 @@ $(".theme-switch").click(function () {
 // Website Blocker For Dad
 if (localStorage.getItem("websiteBlockerForDad") === "yes") {
 
-  location.reload("about:newtab");
+  location.reload("about:blank");
 
 }
