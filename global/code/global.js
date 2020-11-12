7cdf5b2d7b99e452da0501e4f3ddfaafc5af6f4d
Cyber Coder <cyberelectro101@gmail.com>
2020-11-11 23:33:09 -0500

    Popup changes.
    Style and functionality changes for popups. (Maybe) bug fix for Google Analytics accept cookies popup. File name typo.

--- global/code/global.js
+++ global/code/global.js
@@ -1,6 +1,7 @@
-// Google Analytics - Global site tag (gtag.js)
-function importGoogleAnalytics() {
+// Loaded Popups
+if (localStorage.getItem("acceptedCookies") === "yes") {
 
+  // Google Analytics - Global site tag (gtag.js)
   (function (i, s, o, g, r, a, m) {
 
     i["GoogleAnalyticsObject"] = r;
@@ -20,7 +21,7 @@ function importGoogleAnalytics() {
 
     m.parentNode.insertBefore(a, m);
 
-  })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+  })(window, document, "script", "https://www.googletagmanager.com/gtag/js?id=UA-176695618-1", "ga");
 
   ga("create", "UA-XXXXX-Y", "auto");
 
@@ -38,34 +39,65 @@ function importGoogleAnalytics() {
 
   gtag("config", "UA-176695618-1");
 
-}
+  // Google Analytics - Global site tag (gtag.js)
+  (function (i, s, o, g, r, a, m) {
 
-// Loaded Popups
-if (localStorage.getItem("acceptedCookies") === "yes") {
+    i["GoogleAnalyticsObject"] = r;
 
-  // Google Analytics - Import
-  importGoogleAnalytics();
+    i[r] = i[r] || function () {
 
-  // Accept Cookies - Declined
-  localStorage.setItem("acceptedCookies", "yes");
+      (i[r].q = i[r].q || []).push(arguments)
+
+    }, i[r].l = 1 * new Date();
+
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+
+    a.async = 1;
+
+    a.src = g;
+
+    m.parentNode.insertBefore(a, m);
+
+  })(window, document, "script", "https://www.googletagmanager.com/gtag/js?id=UA-176695618-1", "ga");
+
+  ga("create", "UA-XXXXX-Y", "auto");
+
+  ga("send", "pageview");
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+
+    dataLayer.push(arguments);
+
+  }
+
+  gtag("js", new Date());
+
+  gtag("config", "UA-176695618-1");
 
 } else {
 
   Swal.fire({
 
-    html: "This site uses Google Analytics (which uses cookies) to improve your experience, if you continue you are agreeing to these terms.",
-
+    html: "This site uses cookies for analytic purposes, if you accept or close this you are agreeing to these terms.",
+    toast: true,
+    position: "bottom",
+    width: "100vw",
+    padding: "1.25rem 10% 1.25rem",
     showDenyButton: true,
-    confirmButtonText: "<span class=\"swal2-beta-popup-agree-button-text\">I Agree</span>",
-    denyButtonText: "<span class=\"swal2-beta-popup-decline-button-text\">Decline</span>",
+    confirmButtonText: "<span class=\"swal2-accept-cookies-agree-button-text\">I Agree</span>",
+    denyButtonText: "<a href=\"about:blank\" class=\"swal2-accept-cookies-decline-button-text\">Decline</span>",
     confirmButtonColor: "#d9fdde",
-    denyButtonColor: "#ffe3e3"
+    denyButtonColor: "#ffe3e3",
+    customClass: {
+      container: "swal-2-accept-cookies-background-opacity"
+    }
 
   }).then((result) => {
 
     if (result.isDenied) {
-
-      location.replace("about:blank");
 
     } else {
 
@@ -73,27 +105,13 @@ if (localStorage.getItem("acceptedCookies") === "yes") {
       importGoogleAnalytics();
 
       // Accept Cookies - Accepted
-      localStorage.setItem("acceptedCookies", "yes");
+      // localStorage.setItem("acceptedCookies", "yes");
 
     }
 
   })
 
 }
-
-// Footer - Copy URL's URL Copier Popout
-$(".footer-icon-copy-url").click(function () {
-
-  Swal.fire({
-
-    title: "<img src=\"/global/images/link-transparent-icon.svg\" class=\"footer-icon-link-copy-me\"> Copy Me",
-    html: "https://eaganfarlin.com",
-    showConfirmButton: true,
-    confirmButtonText: "Done"
-
-  })
-
-});
 
 // Loading Screen - Fades Out
 $("#loading-screen").fadeOut(1000);
@@ -144,5 +162,19 @@ $(".theme-switch").click(function () {
   localStorage.setItem("theme", theme);
 
   applyTheme(theme);
+
+});
+
+// Footer - Copy URL's URL Copier Popout
+$(".footer-icon-copy-url").click(function () {
+
+  Swal.fire({
+
+    title: "<img src=\"/global/images/link-transparent-icon.svg\" class=\"footer-icon-link-copy-me\"> Copy Me",
+    html: "https://eaganfarlin.com",
+    showConfirmButton: true,
+    confirmButtonText: "Done"
+
+  })
 
 });
