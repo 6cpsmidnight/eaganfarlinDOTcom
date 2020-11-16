46c10d015b39bf52b2bfb5e5e52a696458919d8f
Cyber Coder <cyberelectro1012@gmail.com>
2020-11-15 19:37:18 -0500

    Added responsive bottom page padding for accept cookies popup.
    Added responsive bottom page padding for accept cookies popup. Changed accept cookies text. Fixed text selection color for accept cookies popup. Organized CSS a little better.

--- global/code/global.js
+++ global/code/global.js
@@ -14,21 +14,21 @@ if (localStorage.getItem("acceptedCookies") !== "yes") {
 
   Swal.fire({
 
-    html: "This site uses cookies for analytic purposes, by continuing or closing this you are agreeing to these terms.",
+    html: "<span class=\"swal2-accept-cookies-text\">This site uses cookies to see how many lovely tourists there are! By continuing with your life or closing this you are agreeing to these terms.</span>",
     toast: true,
     position: "bottom",
     width: "100vw",
     padding: "1.25rem 10% 1.25rem",
     showDenyButton: true,
     showCancelButton: true,
-    confirmButtonText: "<span class=\"swal2-accept-cookies-agree-button-text\">I Agree</span>",
-    denyButtonText: "<a href=\"about:blank\" class=\"swal2-accept-cookies-decline-button-text\">Decline</span>",
-    cancelButtonText: "<span class=\"swal2-accept-cookies-x-button-text\">X</span>",
+    confirmButtonText: "<span class=\"swal2-accept-cookies-agree-button-text swal2-accept-cookies-text\">I Agree</span>",
+    denyButtonText: "<a href=\"about:blank\" class=\"swal2-accept-cookies-decline-button-text swal2-accept-cookies-text\">Decline</span>",
+    cancelButtonText: "<span class=\"swal2-accept-cookies-x-button-text swal2-accept-cookies-text\">X</span>",
     confirmButtonColor: "#d9fdde",
     denyButtonColor: "#ffe3e3",
     cancelButtonColor: "var(--secondary-color)",
     customClass: {
-      container: "swal2-accept-cookies-background-opacity",
+      container: "swal2-accept-cookies",
       actions: "vertical-buttons",
       cancelButton: "vertical-buttons-top-margin",
       denyButton: "vertical-buttons-top-margin"
@@ -40,12 +40,30 @@ if (localStorage.getItem("acceptedCookies") !== "yes") {
 
     } else {
 
+      // Sweet Alert Accepted Cookies - Remove Bottom Page Padding
+      $(".sweet-alert-accept-cookies-bottom-page-padding").css("height", "0");
+
+      $(document).scroll(function () {
+
+        $(".sweet-alert-accept-cookies-bottom-page-padding").css("height", "0");
+    
+      });
+
       // Accept Cookies - Accepted
       localStorage.setItem("acceptedCookies", "yes");
 
     }
 
   })
+
+  // Sweet Alert Accepted Cookies - Bottom Page Padding
+  $(".sweet-alert-accept-cookies-bottom-page-padding").css("height", $(".swal2-accept-cookies").height());
+
+  $(document).scroll(function () {
+
+    $(".sweet-alert-accept-cookies-bottom-page-padding").css("height", $(".swal2-accept-cookies").height());
+
+  });
 
 }
 
